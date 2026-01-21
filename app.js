71bf9cf19c40888751dbf000060f54d050549465
Your Name <your.email@example.com>
2026-01-21 19:41:49 +0700

    Fix: update app.js menambahkan keamanannya

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const connectDB = require('./config/database')
 const authRoutes = require('./routes/authRoutes')
 const filmRoutes = require('./routes/filmRoutes')
+const cors = require('cors')
 const fs = require('fs')
 require('dotenv').config()
 const app = express()
@@ -12,6 +13,12 @@ connectDB()
 if (!fs.existsSync('uploads')) {
   fs.mkdirSync('uploads', { recursive: true });
 }
+
+app.use(cors({
+  origin: '*', // Bisa dari mana saja
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json())
 app.use('/uploads', express.static('uploads'));
