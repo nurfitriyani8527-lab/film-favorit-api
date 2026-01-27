3f3f5b27cac60628ae770f0dbc8e8a0ff38788b7
Your Name <your.email@example.com>
2026-01-27 13:58:24 +0700

    Fix: something problem with logout feature

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
+const authMiddleware = require('../middleware/auth')
 
 router.post('/register',authController.Register)
 
 router.post('/login',authController.login)
 
-router.delete('/logout',authController.logout)
+router.delete('/logout', authMiddleware, authController.logout)
 
 module.exports = router
